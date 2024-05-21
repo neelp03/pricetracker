282bba56ea5c6ea0772718ae55939848d86a4fd0
Neel Patel <np.patelneel@gmail.com>
2024-05-21 11:44:21 -0700

    Scrape description and other product related info

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
-import { extractPrice } from "../utils";
+import { extractCurrency, extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
-  if(!url) return;
+  if (!url) return;
 
   //BrightData proxy configuration
   const username = String(process.env.BRIGHTDATA_USERNAME);
@@ -14,12 +14,12 @@ export async function scrapeAmazonProduct(url: string) {
   const options = {
     auth: {
       username: `${username}-session-${session_id}`,
-      password: password
+      password: password,
     },
-    host: 'brd.superproxy.io',
+    host: "brd.superproxy.io",
     port,
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  };
 
   try {
     const response = await axios.get(url, options);
@@ -27,17 +27,63 @@ export async function scrapeAmazonProduct(url: string) {
 
     const title = $("#productTitle").text().trim();
     const currentPrice = extractPrice(
-      $('.a-price-whole , .a-price-fraction'),
+      $(".a-price-whole , .a-price-fraction"),
+      $("a.size.base.a-color-price"),
+      $(".priceToPay span.a-price-whole , .priceToPay span.a-price-fraction")
     );
 
     const originalPrice = extractPrice(
-      $('#priceblock_ourprice'),
-      $('.a-price.a-text-price span.a-offscreen'),
-      $('#listPrice'),
-      $('#priceblock_dealprice'),
-      $('.a-size-base.a-color-price.a-text-strike')
+      $("#priceblock_ourprice"),
+      $(".a-price.a-text-price span.a-offscreen"),
+      $("#listPrice"),
+      $("#priceblock_dealprice"),
+      $(".a-size-base.a-color-price.a-text-strike")
     );
-    console.log({ title, currentPrice })
+
+    const outOfStock = $("#availability span")
+      .text()
+      .toLowerCase()
+      .includes("unavailable");
+
+    const images =
+      $("#imgBlkFront").attr("data-a-dynamic-image") ||
+      $("#landingImage").attr("data-a-dynamic-image") ||
+      "{}";
+
+    const imageUrl = Object.keys(JSON.parse(images));
+
+    const currency = extractCurrency($(".a-price-symbol"));
+    const discountRate = $(".savingsPercentage").text().replace(/[-%]/g, "");
+
+    const description: string[] = [];
+    $("#feature-bullets ul li").each((_, el) => {
+      description.push($(el).text().trim());
+    });
+
+    const category: string[] = [];
+    $("#wayfinding-breadcrumbs_feature_div .a-link-normal").each((_, el) => {
+      category.push($(el).text().trim());
+    });
+
+    const reviewsCount = $("#acrCustomerReviewText").text().split(" ")[0];
+    // only get the first 3 characters of the stars
+    const stars = $("#acrPopover").attr("title");
+
+    const data = {
+      url,
+      currency: currency || "$",
+      image: imageUrl[0],
+      title,
+      currentPrice: Number(currentPrice),
+      originalPrice: Number(originalPrice),
+      priceHistory: [],
+      discountRate: Number(discountRate),
+      category: category,
+      reviewsCount: Number(reviewsCount),
+      stars: String(stars).substring(0, 3),
+      isOutofStock: outOfStock,
+      description,
+    };
   } catch (error: any) {
     throw new Error(`failed to scrape product: ${error.message}`);
   }
