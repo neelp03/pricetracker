2b59f0f6a1e1dd64376d6615a2f29b9a386086a6
Neel Patel <np.patelneel@gmail.com>
2024-05-21 23:20:16 -0700

    Begin track modal implementation

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,8 +1,51 @@
-import React from 'react'
+"use client"
+
+import { Description, Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from "@headlessui/react"
+import { Fragment, useState } from "react"
 
 const Modal = () => {
+  let [isOpen, setIsOpen] = useState(false)
+
+  const openModal = () => setIsOpen(true)
+  const closeModal = () => setIsOpen(false)
+
   return (
-    <div>Modal</div>
+    <>
+      <button type="button" className='btn' onClick={openModal}>
+        Track
+      </button>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" onClose={closeModal} className='dialog-container'>
+          <div className="min-h-screen px-4 text-center">
+            <TransitionChild
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <DialogPanel className='fixed inset-0'/>
+            </TransitionChild>
+
+            <TransitionChild
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="dialog-content">
+                Test
+              </div>
+            </TransitionChild>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
   )
 }
 
